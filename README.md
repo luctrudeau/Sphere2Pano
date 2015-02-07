8e46b88de7987d1d7d34ade3fbd18c8b14ef6884
Luc Trudeau <luc.trudeau@etsmtl.ca>
2015-02-06 20:57:41 -0500

    Finished calibration, added panorama mode and rift mode

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Sphere2Pano
 
 Sphere2Pano is a simple tool to convert image taken from mirrored-spheres to panoramas.
+
+Dependencies:
+    TODO
+
+Start:
+    Simply run python Server.py 
--- Server.py
+++ Server.py
@@ -1,11 +1,16 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import cv2
+import numpy as np
+import urlparse
+from math import sqrt
 
 # ======================== CONFIG =========================
 PORT = 8360
 HOSTNAME = '127.0.0.1'
 DEVICE = 0 # TODO PUT THIS IN UI
+DEST_WIDTH = 2048 # TODO PUT THIS IN UI
+DEST_HEIGHT = 512 # TODO PUT THIS IN UI
 # =========================================================
 
 BOUNDARY = '--ILOVETOPARTYWITHMJPG'
@@ -46,6 +51,81 @@ def live(self):
 			raise e
 			break
 
+def panorama(self):
+	self.wfile.write((
+		"Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0\r\n"
+		"Connection: close\r\n"
+		"Pragma: no-cache\r\n"
+		"Expires: Mon, 3 Jan 2000 12:34:56 GMT\r\n"
+		"Content-Type: multipart/x-mixed-replace; boundary=%s\r\n\r\n" % BOUNDARY
+	))
+
+	while True:
+		try:
+			rc,img = capture.read()
+			if not rc:
+				continue
+			#bigImg = cv2.resize(img, (0, 0), fx=SCALE, fy=SCALE, interpolation=cv2.INTER_CUBIC)
+			img2 = cv2.remap(img,map_x,map_y,cv2.INTER_AREA)
+			rc,cv2mat = cv2.imencode(".jpg",img2)
+			jpeg = cv2mat.tostring() 
+			self.wfile.write((
+				BOUNDARY + "\r\n"
+				"Content-Type: image/jpeg\r\n"
+				"Content-Length: "+str(len(jpeg + "\r\n\r\n"))+"\r\n\r\n"
+				+jpeg + "\r\n\r\n\r\n"
+			))
+		except Exception, e:
+			raise e
+			break
+
+def rift(self):
+	self.wfile.write((
+		"Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0\r\n"
+		"Connection: close\r\n"
+		"Pragma: no-cache\r\n"
+		"Expires: Mon, 3 Jan 2000 12:34:56 GMT\r\n"
+		"Content-Type: multipart/x-mixed-replace; boundary=%s\r\n\r\n" % BOUNDARY
+	))
+
+	while True:
+		try:
+			rc,img = capture.read()
+			if not rc:
+				continue
+			#bigImg = cv2.resize(img, (0, 0), fx=SCALE, fy=SCALE, interpolation=cv2.INTER_CUBIC)
+			remap[511:1023, 0:2048, 0:3] = cv2.remap(img,map_x,map_y,cv2.INTER_AREA)
+			rc,cv2mat = cv2.imencode(".jpg",remap)
+			jpeg = cv2mat.tostring() 
+			self.wfile.write((
+				BOUNDARY + "\r\n"
+				"Content-Type: image/jpeg\r\n"
+				"Content-Length: "+str(len(jpeg + "\r\n\r\n"))+"\r\n\r\n"
+				+jpeg + "\r\n\r\n\r\n"
+			))
+		except Exception, e:
+			raise e
+			break
+			
+
+def buildMap(self, x, y, width, height):
+	global map_x
+	global map_y
+	map_x = np.zeros((DEST_HEIGHT,DEST_WIDTH),np.float32)
+	map_y = np.zeros((DEST_HEIGHT,DEST_WIDTH),np.float32)
+	
+	cx = x + (width / 2)
+	cy = y + (height / 2)
+	R = int(min(width, height) / 2)
+
+	for dy in range(0, int(DEST_HEIGHT-1)):
+		for dx in range(int(DEST_WIDTH-1), 0, -1):
+			r = sqrt(float(dy)/float(DEST_HEIGHT)) * R
+			theta = (float(dx)/float(DEST_WIDTH)) * 2.0 * np.pi
+			xS = cx + r*np.sin(theta)
+			yS = cy - r*np.cos(theta)
+			map_x.itemset(DEST_HEIGHT - (dy+1), DEST_WIDTH - (dx+1), int(xS))
+			map_y.itemset(DEST_HEIGHT - (dy+1), DEST_WIDTH - (dx+1), int(yS))
 
 class CameraRequestHandler(SimpleHTTPRequestHandler):
 	def do_GET(self):
@@ -54,11 +134,27 @@ class CameraRequestHandler(SimpleHTTPRequestHandler):
 			if self.path.endswith('live.mjpg'):
 				self.send_response(200)
 				live(self)
+			elif self.path.endswith('pano.mjpg'):
+				self.send_response(200)
+				panorama(self)
+			elif self.path.endswith('rift.mjpg'):
+				self.send_response(200)
+				rift(self)
 			elif self.path.endswith('jpg'):
 				self.send_response(200)
 				picture(self)
 		else:
 			SimpleHTTPRequestHandler.do_GET(self)
+
+	def do_POST(self):
+		length = int(self.headers.getheader('content-length'))
+		postvars = urlparse.parse_qs(self.rfile.read(length), keep_blank_values=1)
+		buildMap(self, int(postvars['x'][0]), int(postvars['y'][0]), int(postvars['w'][0]), int(postvars['h'][0]))
+
+		self.send_response(303)
+		self.wfile.write('Location: panorama.html')
+		self.end_headers()
+		self.wfile.write(postvars)
 
 def main():
 	global capture
@@ -73,6 +169,8 @@ def main():
 	capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
 
 	global img
+	global remap
+	remap = np.zeros((DEST_HEIGHT + DEST_HEIGHT,DEST_WIDTH, 3),np.uint8)
 
 	try:
 		server = HTTPServer((HOSTNAME,PORT),CameraRequestHandler)
